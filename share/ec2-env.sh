f71c195489c6d0fc137f9ea95dc4f5901f41d537
Hao SUN <xsunsmile@gmail.com>
2011-05-12 10:52:22 +0900

    add ignores

--- share/ec2-env.sh
+++ share/ec2-env.sh
@@ -1,11 +1,1 @@
-export EC2_HOME=~/.ec2
-export EC2_PRIVATE_KEY=$EC2_HOME/your-pk.pem
-export EC2_CERT=$EC2_HOME/your-cert.pem
-# export EC2_URL=https://ec2.ap-northeast-1.amazonaws.com
-export EC2_URL=https://ec2.us-west-1.amazonaws.com
-
-alias  interssh="ssh -F ~/.ssh/config_inters"
-alias  interscp="scp -F ~/.ssh/config_inters"
-
-export EC2_ACCESS_KEY="your-one"
-export EC2_ACCESS_CERT="your-one"
+ec2-env.sh.pri
